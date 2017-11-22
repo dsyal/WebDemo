3acb0377cdec169746fecac6db46225dc4caaa3b
dsyal <johnnycoding@163.com>
2017-11-22 23:24:10 +0800

    first commit

---
+++ ModernJsDemo/index.js
@@ -0,0 +1,10 @@
+// index.js
+import moment from 'moment';
+console.log('hello world');
+console.log(moment().startOf('day').fromNow());
+var name = "Bob",
+	time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
+console.log('test');
+console.log('test02');
+console.log('test03');
